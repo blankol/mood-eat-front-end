0214364d12082745606d667c4e9e5272af1611d4
Annette Coll <annette.coll@outlook.com>
2018-01-23 15:39:41 -0500

    set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render () {
+      return (
+      <div>
+           <Header />
+      </div>
+      )
+  }
+}
+
+export default App 
